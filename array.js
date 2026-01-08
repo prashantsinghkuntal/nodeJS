349bdafe4f3e7ef28677be585e8fd7defb635fe4
prashantsinghkuntal <prashantsinghkuntal007@gmail.com>
2026-01-08 11:12:32 +0530

    this is first nodejs class

---
+++ array.js
@@ -0,0 +1,9 @@
+// arr = [10,20,30,40,50]
+// add element on the front 5 
+// output [5,10,20,30,40,50]
+// to print console.log()
+
+let arr = [10,20,30,40,50];
+arr.unshift(5);
+arr.unshift(10);
+console.log(arr);
