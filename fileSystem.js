f3b1ac4192eabfdd694098c4a9027c6065009e38
prashantsinghkuntal <prashantsinghkuntal007@gmail.com>
2026-01-22 14:42:06 +0530

    new function

--- fileSystem.js
+++ fileSystem.js
@@ -1,17 +1,29 @@
-// const fs= require("fs");
-// fs.writeFile("hello.txt",
-//     "Today we are learning file system",
-//     if(error)=>{
-//         console.log("error in creating the file");
-//         {
-//             console.log("file is created")
-//         }
-//     }
-// )
-FileSystem.readFile("hello.txt", "utf-8", (error,data)=>{
-    if(error){
-        console.log("error in loading")
-    }else{
-        console.log("File Data:" ,data)
+const fs= require("fs").promises;
+fs.writeFile("hello.txt",
+    "Today we are learning file system",
+    if(error)=>{
+        console.log("error in creating the file");
+        {
+            console.log("file is created")
+        }
     }
-})
+)
+// FileSystem.readFile("hello.txt", "utf-8", (error,data)=>{
+//     if(error){
+//         console.log("error in loading")
+//     }else{
+//         console.log("File Data:" ,data)
+//     }
+// })
+
+async function readFile() {
+    try{
+       const data = await fs.readFile("hello.txt", "utf-8");
+       console.log("File Content", data)
+    } catch (error) {
+        console.error("Error in file content", error)
+    }
+    }
+
+
+readFile();
