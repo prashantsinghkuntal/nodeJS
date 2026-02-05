fe3e6c6f6d0ff75eef086cecfa29fdd5dba436dd
Prashant Singh <59342701+prashantsinghkuntal@users.noreply.github.com>
2026-02-05 22:21:35 +0530

    Add basic math functions and example usage

--- module.js
+++ module.js
@@ -1,1 +1,18 @@
 
+// math.js (CommonJS)
+function add(a, b) {
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  module.exports = {
+    add,
+    subtract
+  };
+  
+  // app.js (CommonJS)
+  const math = require('./math');
+  console.log(math.add(5, 3)); // 8
