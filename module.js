29850ae91d3e10b70c800dc3eadd064e0b1af04d
Prashant Singh <59342701+prashantsinghkuntal@users.noreply.github.com>
2026-02-07 08:35:07 +0530

    Add Logger class and export it from module.js

--- module.js
+++ module.js
@@ -30,6 +30,22 @@ export function subtract(a, b) {
 }
 
 // app.mjs (ES Module)
+class Logger {
+  constructor(name) {
+    this.name = name;
+  }
+
+  log(message) {
+    console.log(`[${this.name}] ${message}`);
+  }
+
+  error(error) {
+    console.error(`[${this.name}] ERROR:`, error.message);
+  }
+}
+
+// Exporting a single class
+module.exports = Logger;
 
 
 // logger.js
