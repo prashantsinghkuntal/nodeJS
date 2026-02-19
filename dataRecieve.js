d6cd0b52ef80d31e85c8120ac8b6bc5c28a770d6
Prashant Singh <59342701+prashantsinghkuntal@users.noreply.github.com>
2026-02-19 11:47:22 +0530

    Update dataRecieve.js

--- dataRecieve.js
+++ dataRecieve.js
@@ -6,3 +6,14 @@ const server = http.createServer((req , res) =>{
     if (req.url ==="/"){
         res.end("my home page")
     }
+    else if (req.url ==="/about"){
+            res.end("my about page")
+    }
+    res.end("hello my 1st server")
+
+    })
+
+    server.listen(3000 , ()=>{
+    console.log("my port is runnning on :3000")
+})
+
