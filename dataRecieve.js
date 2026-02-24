9703fcde546222ca7625f698835609a66afb681d
Prashant Singh <59342701+prashantsinghkuntal@users.noreply.github.com>
2026-02-24 21:50:01 +0530

    Update dataRecieve.js

--- dataRecieve.js
+++ dataRecieve.js
@@ -17,3 +17,14 @@ const server = http.createServer((req , res) =>{
     console.log("my port is runnning on :3000")
 })
 
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello World from Express.js!');
+});
+
+app.listen(port, () => {
+  console.log(`Express server running at http://localhost:${port}`);
+});
